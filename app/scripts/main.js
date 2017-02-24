d5a979f00582017d910ae60c86d15f98d545999f
Andrew Morozow <andrey940@gmail.com>
2017-02-24 15:49:40 +0300

    page done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,4 +18,30 @@ $(document).ready(function(){
     arrows: false
   });
 
+  // $(".preview__image").mouseover(function(e){
+  //       $(".preview-video-box").addClass("flex");  
+  //   })
+
+  // $(".preview__image").mouseout(function(e){
+  //     $(".preview-video-box").removeClass("flex");  
+  // })
+
+  // $(".preview__image").mouseover(function(e){
+  //       $(".preview-video-box").removeClass("hidden");  
+  //   })
+
+  // $(".preview__image").mouseout(function(e){
+  //     $(".preview-video-box").addClass("hidden");  
+  // })
+
+  // ANKERS
+    $('li[href^="#"]').on("click", function (e) {
+        e.preventDefault();
+
+        elementClick = $(this).attr("href");
+        destination = $(elementClick).offset().top;
+
+        $('body').animate(1000 );
+    });
+
 });
